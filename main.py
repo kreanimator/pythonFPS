845a9c1d0f66a4526fb8974cf72d95d38e076313
kreanimator <backin.v@yandex.ru>
2023-02-27 18:29:54 +0200

    Added textures to the walls Added Sky and floor background

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ from map import *
 from player import *
 from raycasting import *
 from object_renderer import *
+from sprite_object import *
+from object_handler import *
+from weapon import *
 
 
 class Game:
@@ -21,10 +24,14 @@ class Game:
         self.player = Player(self)
         self.object_renderer = ObjectRenderer(self)
         self.raycasting = RayCasting(self)
+        self.object_handler = ObjectHandler(self)
+        self.weapon = Weapon(self)
 
     def update(self):
         self.player.update()
         self.raycasting.update()
+        self.object_handler.update()
+        self.weapon.update()
         pg.display.flip()
         self.delta_time = self.clock.tick(FPS)
         pg.display.set_caption(f'{self.clock.get_fps() :.1f}')
@@ -32,6 +39,7 @@ class Game:
     def draw(self):
         #self.screen.fill('black')
         self.object_renderer.draw()
+        self.weapon.draw()
         #self.map.draw()
         #self.player.draw()
 
@@ -40,6 +48,7 @@ class Game:
             if event.type == pg.QUIT or (event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE):
                 pg.quit()
                 sys.exit()
+            self.player.single_fire_event(event)
 
     def run(self):
         while True:
